94de801c2f66fc67d1dfe5c2176680ae516f0f03
Gerred Dillon <hello@gerred.com>
2015-09-19 20:08:26 -0700

    Update docs a bit

--- drivers/drivers.go
+++ drivers/drivers.go
@@ -1,6 +1,6 @@
 package drivers
 
-// The Driver interface is the set of functions required to start and verify a Kubernetes cluster. Inspired by the steps used in kube-up.sh
+// The Driver interface is the set of functions required to start, manage, and verify a Kubernetes cluster. Inspired by the steps used in kube-up.sh, with extensions for management and scaling of clusters.
 type Driver interface {
 	GenerateCerts()
 	GetTokens()
