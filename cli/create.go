ac16b308f9f3ca57f59ab3f505818118cb7afe5f
Gerred Dillon <hello@gerred.com>
2015-09-19 19:21:25 -0700

    Begin initial framework for craete command

--- cli/create.go
+++ cli/create.go
@@ -14,23 +14,21 @@
 
 package cli
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/cloudflare/cfssl/log"
+	"github.com/spf13/cobra"
+)
 
-// KubeClusterCmd is the root command. Attach all other commands to this.
-var KubeClusterCmd = &cobra.Command{
-	Use:   "kube-cluster",
-	Short: "kube-cluster provisions, scales, and manages kubernetes environments",
-	Long:  "kube-cluster provisions, scales, and manages kubernetes environments",
-	Run: func(cmd *cobra.Command, args []string) {
-	},
+var createCmd = &cobra.Command{
+	Use:   "create [name]",
+	Short: "Create a Kubernetes cluster with the given name and provider options",
+	Run:   CreateCluster,
 }
 
-// Execute adds all child commands to the root command KubeClusterCmd and sets all flags appropriately.
-func Execute() {
-	addCommands()
-	KubeClusterCmd.Execute()
-}
-
-func addCommands() {
-	KubeClusterCmd.AddCommand(createCmd)
+//CreateCluster creates a new Kubernetes cluster with a provider name and options.
+func CreateCluster(cmd *cobra.Command, args []string) {
+	if len(args) < 1 {
+		cmd.Usage()
+		log.Error("name needs to be provided")
+	}
 }
